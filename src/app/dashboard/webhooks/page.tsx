192794e4146246984608c610492bda27a0363b83
Divyam Singh <divyamsingh1918@gmail.com>
2026-03-13 15:03:10 +0530

    updateS

---
+++ src/app/dashboard/webhooks/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+//to do make a basic page for webhook submissions and fetching
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
